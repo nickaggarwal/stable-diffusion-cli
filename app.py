9d1ed2aeda3d3565ed5cc2524ff01a88045cec91
Peakperformer <peakperformer@MacBook-Pro-10.local>
2024-01-30 16:15:18 -0800

    changes

---
+++ app.py
@@ -0,0 +1,26 @@
+from diffusers import StableDiffusionPipeline
+import torch
+from io import BytesIO
+import base64
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-1",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+            device_map='auto'
+        )
+
+
+    def infer(self, inputs):
+        prompt = inputs["prompt"]
+        image = self.pipe(prompt).images[0]
+        buff = BytesIO()
+        image.save(buff, format="JPEG")
+        img_str = base64.b64encode(buff.getvalue()).decode()
+        return { "generated_image_base64" : img_str }
+        
+    def finalize(self):
+        self.pipe = None
